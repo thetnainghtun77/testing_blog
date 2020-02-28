2746bb491eaca41791ae19785ca00c6956b74d04
swehponenaing <swehponenaing97@gmail.com>
2020-02-28 17:48:39 +0630

    Add new route

--- routes/web.php
+++ routes/web.php
@@ -99,3 +99,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/addquizzes','BackendController@addquizzes')->middleware('role:Trainer');
 Route::get('/grades','BackendController@grades')->middleware('role:Mentor');
+
+Route::get('/mine', function(){
+	echo "I am mine ";
+})
